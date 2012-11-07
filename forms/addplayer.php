d1f473a8d686b27cb38094d1a04937f43c16d60f
Shreyas <rhodes.unci@gmail.com>
2012-11-07 13:53:27 -0800

    adding the first form file

---
+++ forms/addplayer.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Zakumi | add players to the database</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+        <h1>Add Players</h1>
+
+        <form class="generalform" action="#">
+            <ul>
+                <li>
+                    <label for="formfield-playername">Player Name</label>
+                    <input type="text" id="formfield-playername" />
+                </li>
+                <li>
+                    <label for="formfield-playerdob">Date of Birth</label>
+                    <input type="date" id="formfield-playerdob" />
+                </li>
+                <li>
+                    <label for="formfield-playerheight">Height</label>
+                    <input type="text" id="formfield-playername" />
+                </li>
+                <li>
+                    <label for="formfield-playerweight">Weight</label>
+                    <input type="text" id="formfield-playername" />
+                </li>
+                <li>
+                    <label for="formfield-playernationality">Nationality</label>
+                    <input type="text" id="formfield-playername" />
+                </li>
+                <li>
+                    <label for="formfield-playerssn">SSN</label>
+                    <input type="text" id="formfield-playername" />
+                </li>
+                <li>
+                    <label for="formfield-playersalary">Salary</label>
+                    <input type="text" id="formfield-playername" />
+                </li>
+                <li>
+                    <label for="formfield-playerstreet">Street</label>
+                    <input type="text" id="formfield-playername" />
+                </li>
+                <li>
+                    <label for="formfield-playercity">City</label>
+                    <input type="text" id="formfield-playername" />
+                </li>
+                <li>
+                    <label for="formfield-playercountry">Country</label>
+                    <input type="text" id="formfield-playername" />
+                </li>
+            </ul>
+        </form>
+
+
+        <script src="js/vendor/jquery-1.8.2.min.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+    </body>
+</html>
