a290b64e6e77b49191e93472e81c0dc81dd39543
Shreyas <rhodes.unci@gmail.com>
2012-11-22 00:06:08 -0800

    started developing via template page

--- index.php
+++ index.php
@@ -3,41 +3,13 @@
     $_SESSION['username'] = 'shreyas';
     $_SESSION['authuser'] = 1;
 ?>
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Zakumi | Home</title>
-        <meta name="description" content="football database">
-        <meta name="viewport" content="width=device-width">
 
-        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/main.css">
-        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
-    </head>
-    <body>
-        <!--[if lt IE 7]>
-            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
-        <![endif]-->
+<!-- Add your site or application content here -->
+<?php include 'templates/pagetop.php' ?>
+<?php
+    echo 'Zakumi goes live for ';
+    echo FAVPLAYER;
+?>
 
-        <!-- Add your site or application content here -->
-        <p>This is the project website for Zakumi football. Course Info 257. By Aisha Kigongo, Ashley DeSouza, Shreyas</p>
-        <?php
-            echo 'Zakumi goes live for ';
-            echo FAVPLAYER;
-        ?>
-
-        <a href="forms/report_playersalary.php">report</a>
-
-        <script src="js/vendor/jquery-1.8.2.min.js"></script>
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
-
-    </body>
-</html>
+<?php include 'templates/pagebottom.php' ?>
---
+++ templates/pagebottom.php
@@ -0,0 +1,8 @@
+<a href="forms/report_playersalary.php">report</a>
+
+        <script src="js/vendor/jquery-1.8.2.min.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+    </body>
+</html>
