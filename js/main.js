98feb6b02a40df4c3cfa7712d7188f95070031f9
Shreyas <rhodes.unci@gmail.com>
2012-11-23 00:19:46 -0800

    circle animation working

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var ZAKUMI = {}; //defining the global namespace
+var ZAKUMI = ZAKUMI | {}; //defining the global namespace
 
 ZAKUMI = (function(){
 
@@ -6,20 +6,100 @@ ZAKUMI = (function(){
 		console.log("initiated");
 
 		initCircleInteractions();
+
 	}
 
 
 	function initCircleInteractions()
 	{
 		$('div.circle').mouseover(function() {
-	        $('div.outer-circle').addClass('hover');
-	        $('div.middle-circle').addClass('hover');
+			var elem = jQuery(this);
+	        elem.find('div.outer-circle').addClass('hover');
+	        elem.find('div.middle-circle').addClass('hover');
 	    });
 	    $('div.circle').mouseout(function() {
-	        $('div.outer-circle').removeClass('hover');
-	        $('div.middle-circle').removeClass('hover');
+	    	var elem = jQuery(this);
+	        elem.find('div.outer-circle').removeClass('hover');
+	        elem.find('div.middle-circle').removeClass('hover');
 	    });
+
+
+
+
+	    $("#mainCircleContainer").mouseover(function(){
+	        TweenLite.to($("#outerCircle"), 0.4, 
+	        	{
+	        		css: {width:146,height:146,marginLeft:-75, marginTop:-75}, 
+	        		ease:Power2.easeOut, 
+	        		onComplete: function(){
+	        			calculatePositions();
+	        		}
+	        	});
+	    });
+
+
+	    $("#mainCircleContainer").mouseleave(function(){
+	    	TweenLite.to($('#outerCircle .resultCircle'),0.3,{css:{autoAlpha:0,scaleX:0.1,scaleY:0.1}});
+	        TweenLite.to($("#outerCircle"), 0.5, {css: {width:80,height:80,marginLeft:-42, marginTop:-42}, ease:Expo.easeOut, delay:0.4, overwrite:"all"});
+	    });
+
+
+	    // $(".draggableCircles").live('click',function(){
+	    //     addCircle($(this),false);
+	    // });
+
+
+
+	    $(".draggableCircles").draggable({
+	    	cancel: "a.ui-icon",
+	    	revert: "invalid",
+	    	helper: "clone",
+	    	zIndex: 120,
+	    	cursor: "move"
+		});
+
+		$("#mainCircleContainer").droppable({
+		   	accept: ".draggableCircles",
+		    activeClass: "ui-state-highlight",
+		    drop: function( event, ui ) {
+		    	console.log("on drop");
+		    	addCircle( ui.draggable,true ); 
+		    }
+
+		});
+
+
+
+		function addCircle($item){
+			var id = $item.attr('class');
+        	id = String(id).split(' ');
+        	id = id[1];
+        	$('#outerCircle').append("<div id='"+id+"' class='resultCircle'></div>");
+        	calculatePositions();
+
+		}
+
+		function calculatePositions(){
+			var radius  = 40+16; // offset here to add some spacing
+		    var num     = $("#outerCircle").children().length;
+		    var dividers= 360/num;
+		    var center  = 58;
+		    var theta   = 0.0;
+
+		    for(var i=0;i<num;i++){
+		        var x0 = Math.round(center+radius*Math.cos(theta));
+		        var y0 = Math.round(center+radius*Math.sin(theta));
+		        $("#outerCircle :nth-child("+(i+1)+")").css({'left':x0,'top':y0});      
+		        var radians = dividers * (Math.PI / 180);
+		        theta +=  radians;
+		    }
+
+		    TweenLite.to($('#outerCircle .resultCircle'),0.2,{css:{autoAlpha:1,scaleX:1,scaleY:1}});
+		}
 	}
+
+	
+
 
 	return {
 		'init' : init
@@ -31,11 +111,4 @@ ZAKUMI = (function(){
 jQuery(document).ready(function()
 {
 	ZAKUMI.init();
-});
-
-
-
-
-$(function() {
-    
 });
--- team.php
+++ team.php
@@ -6,13 +6,40 @@
 <!-- Add your site or application content here -->
 <?php include 'templates/pagetop.php' ?>
 <?php include 'templates/header.php' ?>
+
+
+
+
+
 <div id="wrapper_middle">
 	<div class="wrapper_content">
-		<div class="circle">
-		    <div class="outer-circle"></div>
-		    <div class="middle-circle"></div>
-		    <div class="inner-circle">Team1</div>
+
+
+		<div id="circlesArray">
+
+	        <div class="draggableCircles red" ></div>
+
+	        <div class="draggableCircles purple" ></div>
+
+	        <div class="draggableCircles green" ></div>
+
+	        <div class="draggableCircles black" ></div>
+
+	        <div class="draggableCircles yellow" ></div>
+
+	        <div class="draggableCircles pink" ></div>
+
 		</div>
+
+
+		<div id="mainCircleContainer">
+		    <div id="dragWindow"></div>
+		    <div id="smallCircle">
+		        <label id="circleLabel">Contacts</label>
+		    </div>
+		    <div id="outerCircle"></div>
+		</div>
+
 
 		<div class="circle">
 		    <div class="outer-circle"></div>
@@ -23,16 +50,32 @@
 		<div class="circle">
 		    <div class="outer-circle"></div>
 		    <div class="middle-circle"></div>
-		    <div class="inner-circle">Team1</div>
+		    <div class="inner-circle">Team2</div>
 		</div>
 
 		<div class="circle">
 		    <div class="outer-circle"></div>
 		    <div class="middle-circle"></div>
-		    <div class="inner-circle">Team1</div>
+		    <div class="inner-circle">Team3</div>
+		</div>
+
+		<div class="circle">
+		    <div class="outer-circle"></div>
+		    <div class="middle-circle"></div>
+		    <div class="inner-circle">Team4</div>
 		</div>
 	</div>
 </div>
 
+	<script src="js/vendor/jquery-1.8.2.min.js"></script>
+	<script src="js/vendor/jquery-ui-1.9.1.custom.min.js"></script>
+	<script src="js/vendor/timelinelite.js"></script>
+	<script src="js/vendor/easypack.js"></script>
+	<script src="js/vendor/timelinemax.js"></script>
+	<script src="js/vendor/tweenmax.js"></script>
+	<script src="js/vendor/animationcircle.js"></script>
+ 
+	<script src="js/main.js"></script>
 
-<?php include 'templates/pagebottom.php' ?>
+</body>
+</html>
