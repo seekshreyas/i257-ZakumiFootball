97d53e7c6539eec5223e247b7f7a52496bb4715f
Shreyas <rhodes.unci@gmail.com>
2012-11-22 23:18:52 -0800

    footer error removed

--- js/main.js
+++ js/main.js
@@ -1,1 +1,41 @@
+var ZAKUMI = {}; //defining the global namespace
 
+ZAKUMI = (function(){
+
+	var init = function(){
+		console.log("initiated");
+
+		initCircleInteractions();
+	}
+
+
+	function initCircleInteractions()
+	{
+		$('div.circle').mouseover(function() {
+	        $('div.outer-circle').addClass('hover');
+	        $('div.middle-circle').addClass('hover');
+	    });
+	    $('div.circle').mouseout(function() {
+	        $('div.outer-circle').removeClass('hover');
+	        $('div.middle-circle').removeClass('hover');
+	    });
+	}
+
+	return {
+		'init' : init
+	}
+
+})();
+
+
+jQuery(document).ready(function()
+{
+	ZAKUMI.init();
+});
+
+
+
+
+$(function() {
+    
+});
---
+++ team.php
@@ -0,0 +1,38 @@
+<?php
+    session_start();
+?>
+
+
+<!-- Add your site or application content here -->
+<?php include 'templates/pagetop.php' ?>
+<?php include 'templates/header.php' ?>
+<div id="wrapper_middle">
+	<div class="wrapper_content">
+		<div class="circle">
+		    <div class="outer-circle"></div>
+		    <div class="middle-circle"></div>
+		    <div class="inner-circle">Team1</div>
+		</div>
+
+		<div class="circle">
+		    <div class="outer-circle"></div>
+		    <div class="middle-circle"></div>
+		    <div class="inner-circle">Team1</div>
+		</div>
+
+		<div class="circle">
+		    <div class="outer-circle"></div>
+		    <div class="middle-circle"></div>
+		    <div class="inner-circle">Team1</div>
+		</div>
+
+		<div class="circle">
+		    <div class="outer-circle"></div>
+		    <div class="middle-circle"></div>
+		    <div class="inner-circle">Team1</div>
+		</div>
+	</div>
+</div>
+
+
+<?php include 'templates/pagebottom.php' ?>
