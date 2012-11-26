c89423832cdd712830702b6d94d2f6e7448e74ca
ashley.desouza <ashley.desouza@ischool.berkeley.edu>
2012-11-25 17:10:46 -0800

    Added csv files for Team, Manager and Player upload. Player csv file is to be updates. Added php files for bulk upload and for query.

---
+++ php_bulk_upload/Managers_Bulk_Upload.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // Make a MySQL Connection
+        mysql_connect("54.243.36.26","dbuser","deer") or die(mysql_error());
+        mysql_select_db("ZAKUMI") or die(mysql_error());
+        
+        //Import data from .csv file
+        $handle = fopen("managers.csv", "r");     
+        
+        //Insert Query
+        while (($data = fgetcsv($handle, ",")) !== FALSE){
+        mysql_query("INSERT INTO MANAGERS (NATIONALITY, NAME, SALARY, STREET, CITY, COUNTRY, MANAGERPHOTO) VALUES('".$data[0]."','".$data[1]."','".$data[2]."','".$data[3]."','".$data[4]."','".$data[5]."','".$data[6]."')") or die(mysql_error()); 
+        echo "Row Successfully created in db!!";
+        echo '</br>';
+        }
+        
+        fclose($handle);        
+        ?>
+    </body>
+</html>
+
---
+++ php_bulk_upload/Players_Query_Page.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // Make a MySQL Connection
+        mysql_connect("54.243.36.26","dbuser","deer") or die(mysql_error());
+        mysql_select_db("ZAKUMI") or die(mysql_error());
+        
+        //Query the Database
+        $query = mysql_query("SELECT * FROM PLAYERS");
+        
+        //Fetch each row from the query result
+        while (($row = mysql_fetch_row($query))!== FALSE){
+            echo('<ul><li>'.$row[0].'<li>'.$row[1].'<li>'.$row[2].'<li>'.$row[3].'<li>'.$row[4].'<li>'.$row[5].'<li>'.$row[6].'<li>'.$row[7]).'</ul>';
+            echo "<img src='".$row[8]."' alt='Image' />";
+            echo("<br/>");
+        }    
+        ?>
+    </body>
+</html>
