0feb72a772ac0331ab8a6e4bd6018e89f0395e4b
Shreyas <rhodes.unci@gmail.com>
2012-11-29 00:32:28 -0800

    checking for automatic github updates

--- sort.php
+++ sort.php
@@ -2,7 +2,7 @@
     session_start();
     include 'config.php'; //including the configuration file
 
-    $pageTitle = 'SORT | team page';
+    $pageTitle = 'SORT | SHREYAS';
 ?>
 <!-- Add your site or application content here -->
 <?php include 'templates/pagetop.php' ?>
