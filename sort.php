ffa6c3a934342fe6bf6867dd52cb625b1a191fd9
Shreyas <rhodes.unci@gmail.com>
2012-11-29 00:40:36 -0800

    minor change

--- sort.php
+++ sort.php
@@ -2,7 +2,7 @@
     session_start();
     include 'config.php'; //including the configuration file
 
-    $pageTitle = 'SORT | ashley';
+    $pageTitle = 'SORT | shreyas';
 ?>
 <!-- Add your site or application content here -->
 <?php include 'templates/pagetop.php' ?>
